eaf5b2048798a494e4a6f6bb2d78b95b608626a2
Andrew Buchan <andyhasit@gmail.com>
2026-02-11 19:44:28 +0000

    leaner code

--- frameworks/non-keyed/wallace/src/main.jsx
+++ frameworks/non-keyed/wallace/src/main.jsx
@@ -1,121 +1,5 @@
 import { mount } from "wallace";
 
-const A = [
-  "pretty",
-  "large",
-  "big",
-  "small",
-  "tall",
-  "short",
-  "long",
-  "handsome",
-  "plain",
-  "quaint",
-  "clean",
-  "elegant",
-  "easy",
-  "angry",
-  "crazy",
-  "helpful",
-  "mushy",
-  "odd",
-  "unsightly",
-  "adorable",
-  "important",
-  "inexpensive",
-  "cheap",
-  "expensive",
-  "fancy",
-];
-const C = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
-const N = [
-  "table",
-  "chair",
-  "house",
-  "bbq",
-  "desk",
-  "car",
-  "pony",
-  "cookie",
-  "sandwich",
-  "burger",
-  "pizza",
-  "mouse",
-  "keyboard",
-];
-
-function random(max) {
-  return Math.round(Math.random() * 1000) % max;
-}
-
-function buildData(count) {
-  const data = new Array(count);
-  for (let i = 0; i < count; i++) {
-    data[i] = {
-      id: nextId++,
-      label: `${A[random(A.length)]} ${C[random(C.length)]} ${N[random(N.length)]}`,
-    };
-  }
-  return data;
-}
-
-let items = [],
-  selected = 0,
-  nextId = 1;
-const add = () => (items = items.concat(buildData(1000))),
-  clear = () => {
-    items = [];
-    selected = undefined;
-  },
-  update10th = () => {
-    for (let i = 0; i < items.length; i += 10) {
-      items[i].label += " !!!";
-    }
-  },
-  rowCss = (rowId) => (rowId === selected ? "danger" : ""),
-  removeRow = (rowId) => {
-    items.splice(
-      items.findIndex((x) => x.id === rowId),
-      1
-    );
-    root.update();
-  },
-  run = () => {
-    items = buildData(1000);
-    selected = undefined;
-  },
-  runLots = () => {
-    items = buildData(10000);
-    selected = undefined;
-  },
-  selectRow = (rowId) => {
-    selected = rowId;
-    root.update();
-  },
-  swapRows = () => {
-    if (items.length > 998) {
-      const temp = items[1];
-      items[1] = items[998];
-      items[998] = temp;
-    }
-  },
-  buttons = [
-    { id: "run", title: "Create 1,000 rows", cb: run },
-    { id: "runlots", title: "Create 10,000 rows", cb: runLots },
-    { id: "add", title: "Append 1,000 rows", cb: add },
-    { id: "update", title: "Update every 10th row", cb: update10th },
-    { id: "clear", title: "Clear", cb: clear },
-    { id: "swaprows", title: "Swap Rows", cb: swapRows },
-  ].map((button) => {
-    button.callback = (_) => {
-      button.cb();
-      root.update();
-    };
-    return button;
-  });
-
-// Wallace code starts here
-
 const MainView = () => (
   <div unique class="container">
     <div class="jumbotron">
@@ -128,24 +12,28 @@ const MainView = () => (
         </div>
       </div>
     </div>
-    <table class="table table-hover table-striped test-items">
-      <tbody id="tbody">
-        <Row.repeat items={items} />
-      </tbody>
-    </table>
+    <Table.nest ref:table />
   </div>
 );
 
-const Button = ({ id, callback, title }) => (
+const Table = () => (
+  <table unique class="table table-hover table-striped test-items">
+    <tbody id="tbody">
+      <Row.repeat items={items} />
+    </tbody>
+  </table>
+);
+
+const Button = ({ id, cb, title }) => (
   <div class="col-sm-6 smallpad">
-    <button id={id} onClick={callback()} class="btn btn-primary btn-block">
+    <button id={id} onClick={cb()} class="btn btn-primary btn-block">
       {title}
     </button>
   </div>
 );
 
-const Row = ({ id, label }) => (
-  <tr class={rowCss(id)}>
+const Row = ({ id, label }, { props }) => (
+  <tr class={id === selected ? "danger" : ""}>
     <td class="col-md-1">{id}</td>
     <td class="col-md-4">
       <a onClick={selectRow(id)} class="lbl">
@@ -153,7 +41,7 @@ const Row = ({ id, label }) => (
       </a>
     </td>
     <td class="col-md-1">
-      <a class="remove" onClick={removeRow(id)}>
+      <a class="remove" onClick={removeRow(props)}>
         <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
       </a>
     </td>
@@ -161,4 +49,129 @@ const Row = ({ id, label }) => (
   </tr>
 );
 
-const root = mount("main", MainView);
+let items = [],
+  nextId = 1,
+  selected;
+
+const A = [
+    "pretty",
+    "large",
+    "big",
+    "small",
+    "tall",
+    "short",
+    "long",
+    "handsome",
+    "plain",
+    "quaint",
+    "clean",
+    "elegant",
+    "easy",
+    "angry",
+    "crazy",
+    "helpful",
+    "mushy",
+    "odd",
+    "unsightly",
+    "adorable",
+    "important",
+    "inexpensive",
+    "cheap",
+    "expensive",
+    "fancy",
+  ],
+  C = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"],
+  N = [
+    "table",
+    "chair",
+    "house",
+    "bbq",
+    "desk",
+    "car",
+    "pony",
+    "cookie",
+    "sandwich",
+    "burger",
+    "pizza",
+    "mouse",
+    "keyboard",
+  ],
+  random = (max) => Math.round(Math.random() * 1000) % max,
+  buildData = (count) => {
+    const data = new Array(count);
+    for (let i = 0; i < count; i++) {
+      data[i] = {
+        id: nextId++,
+        label: `${A[random(A.length)]} ${C[random(C.length)]} ${N[random(N.length)]}`,
+      };
+    }
+    return data;
+  },
+  removeRow = (row) => {
+    items.splice(items.indexOf(row), 1);
+    updateRows();
+  },
+  selectRow = (rowId) => {
+    selected = rowId;
+    updateRows();
+  },
+  updateRows = () => table.update(),
+  buttons = [
+    {
+      id: "run",
+      title: "Create 1,000 rows",
+      cb: () => {
+        items = buildData(1000);
+        selected = undefined;
+        updateRows();
+      },
+    },
+    {
+      id: "runlots",
+      title: "Create 10,000 rows",
+      cb: () => {
+        items = buildData(10000);
+        updateRows();
+      },
+    },
+    {
+      id: "add",
+      title: "Append 1,000 rows",
+      cb: () => {
+        items = items.concat(buildData(1000));
+        updateRows();
+      },
+    },
+    {
+      id: "update",
+      title: "Update every 10th row",
+      cb: () => {
+        for (let i = 0; i < items.length; i += 10) {
+          items[i].label += " !!!";
+        }
+        updateRows();
+      },
+    },
+    {
+      id: "clear",
+      title: "Clear",
+      cb: () => {
+        items = [];
+        updateRows();
+      },
+    },
+    {
+      id: "swaprows",
+      title: "Swap Rows",
+      cb: () => {
+        if (items.length > 998) {
+          const temp = items[1];
+          items[1] = items[998];
+          items[998] = temp;
+          updateRows();
+        }
+      },
+    },
+  ];
+
+const table = mount("main", MainView).ref.table;
